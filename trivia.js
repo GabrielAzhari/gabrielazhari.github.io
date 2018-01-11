c04ec63e2ed0be520beb44f8babbb826427598ff
GabrielAzhari <35264033+GabrielAzhari@users.noreply.github.com>
2018-01-11 09:41:16 -0500

    updated trivia.js

--- trivia.js
+++ trivia.js
@@ -29,6 +29,18 @@ else if (ans===2){
     }
     document.view.qscore.value=score
 }
+else if (ans===3){ 
+	y = document.getElementById("qans").value
+    if(yy==="A"){
+        document.getElementById("question").value="You are right"
+         score++
+    }
+    else {
+        document.getElementById("question").value="You are wrong"
+        
+    }
+    document.view.qscore.value=score
+}
 	ans++; 
 }
         
@@ -36,15 +48,20 @@ function main(){
 if (i===1){ 
 	document.view.qnum.value=i;
     document.view.qscore.value=0;
-	document.view.question.value ="Which one of these is a percussion instrument? \na)Trumpet \nb)Euphonium \nc)Viola \nd)Glockenspiel";
+	document.view.question.value ="What NBA team is Kyrie Irving on? \na)Golden State Warriors \nb)Duke University \nc)Boston Celtics \nd)Cleveland Cavaliers";
     document.view.qans.value=""
 }
 else if (i===2){ 
 	document.view.qnum.value=i;	
-	document.view.question.value ="Which one of these songs did Ludwig van Beethoven write? \na)Ode to Joy \nb)To a Wild Rose \nc)Rejoice \nd)Hallelujah Chorus";
+	document.view.question.value ="What City has Knicks in its name? \na)Toronto \nb)New York \nc)Chicago \nd)Detroit";
     document.view.qans.value=""
 }
-else if (i===3){
+else if (i===3){ 
+	document.view.qnum.value=i;	
+	document.view.question.value ="What was the Last Team Wayne Gretzky played on? \na)New York Rangers \nb)Edmonton Oilers \nc)Los Angeles Kings \nd)St. Louis Blues";
+    document.view.qans.value=""
+}
+else if (i===4){
     alert("You got"+ " " + Math.round(score/(i-1)*100)  + "% for this Trivia Quiz")
 }
 	i++; 
